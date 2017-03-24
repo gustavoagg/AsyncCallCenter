bf316b552753c6b5cf8c77fd714e4271b242db49
Gustavo Gonzalez <gustavoagg@gmail.com>
2017-03-24 15:13:34 -0400

    Added Request to add calls, and clean code

--- AsyncCallCenter/src/main/java/com/almundo/control/Dispatcher.java
+++ AsyncCallCenter/src/main/java/com/almundo/control/Dispatcher.java
@@ -16,47 +16,45 @@ import com.almundo.model.Operator;
 @Controller
 public class Dispatcher {
 
-    @Autowired	
-    ThreadPoolTaskExecutor threadPool;
-    ConcurrentLinkedQueue<Call> incomingCalls = new ConcurrentLinkedQueue<Call>();
-    List<Future<CallableWorker>> futureList;
-    
-    public void init(){
-    	futureList = new ArrayList<>();
-		Call call = new Call();
-		call.setId(1);
-		call.setTiempo(5000);
-    	incomingCalls.add(call);
-        for(int threadNumber = 1; threadNumber < 11; threadNumber ++){
-            CallableWorker callableTask = new CallableWorker(String.valueOf(threadNumber),incomingCalls);
-            callableTask.ready = true;
-            Future<CallableWorker> result = threadPool.submit(callableTask);
-            futureList.add(result);
-            System.out.println("Created Line: "+threadNumber+" ..waiting for calls");
-        }
-        
-    }
-    
-    public void dispatchCall(Call call){
-    	  System.out.println("New call incoming: "+call.getId()+" ..on hold");          
-    	incomingCalls.add(call);
-    }
-    
-    public String dispatchAll(){
-        String msg = "";
-        List<Future<CallableWorker>> futureList = new ArrayList<>();
-        for(int threadNumber = 0; threadNumber < 12; threadNumber ++){
-            CallableWorker callableTask = new CallableWorker(new Operator(String.valueOf(threadNumber)));
-            Future<CallableWorker> result = threadPool.submit(callableTask);
-            futureList.add(result);
-        }
-          
-        for(Future<CallableWorker> future: futureList){
-            try {
-                msg += future.get().employee.getName() + "#####";
-            } catch (Exception e){}
-        }
-          
-        return msg;
-    }
+	@Autowired
+	ThreadPoolTaskExecutor threadPool;
+	ConcurrentLinkedQueue<Call> incomingCalls = new ConcurrentLinkedQueue<Call>();
+	List<Future<CallableWorker>> futureList;
+
+	public void init() {
+		futureList = new ArrayList<>();
+
+		for (int threadNumber = 1; threadNumber < 11; threadNumber++) {
+			CallableWorker callableTask = new CallableWorker(String.valueOf(threadNumber), incomingCalls);
+			callableTask.ready = true;
+			Future<CallableWorker> result = threadPool.submit(callableTask);
+			futureList.add(result);
+			System.out.println("Created Line: " + threadNumber + " ..waiting for calls");
+		}
+
+	}
+
+	public void dispatchCall(Call call) {
+		System.out.println("New call incoming: " + call.getId() + " ..on hold");
+		incomingCalls.add(call);
+	}
+
+	public String dispatchAll() {
+		String msg = "";
+		List<Future<CallableWorker>> futureList = new ArrayList<>();
+		for (int threadNumber = 0; threadNumber < 12; threadNumber++) {
+			CallableWorker callableTask = new CallableWorker(new Operator(String.valueOf(threadNumber)));
+			Future<CallableWorker> result = threadPool.submit(callableTask);
+			futureList.add(result);
+		}
+
+		for (Future<CallableWorker> future : futureList) {
+			try {
+				msg += future.get().employee.getName() + "#####";
+			} catch (Exception e) {
+			}
+		}
+
+		return msg;
+	}
 }
--- AsyncCallCenter/src/main/java/com/almundo/controller/WebCallController.java
+++ AsyncCallCenter/src/main/java/com/almundo/controller/WebCallController.java
@@ -1,6 +1,9 @@
 package com.almundo.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +24,31 @@ public class WebCallController {
 		return "Open Lines waiting for connection";
 	}
 
-	@RequestMapping("/call/")
-	public String addCall() {
+	@RequestMapping("/calls/{id}")
+	public String makeCalls(@PathVariable("id") int calls) {
+		String message = "";
+		for (int i = 0; i < calls; i++) {
+			message = message + addCall() + "<br />";
+		}
+		return message;
+	}
+
+	private String addCall() {
 		Call call = new Call();
 		callCounter++;
 		call.setId(callCounter);
-		call.setTiempo(5000);
+		call.setTiempo(randTime());
 		dispatcher.dispatchCall(call);
-		return "Added call "+callCounter+" to hold calls";
+		return "Added call " + callCounter + " to hold calls";
+	}
+
+	private long randTime() {
+
+		int min = 5;
+		int max = 10;
+		Random rand = new Random();
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+
+		return randomNum * 1000;
 	}
 }
